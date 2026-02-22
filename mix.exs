a0df150246817445529f2a030d3075e7ab6eb2c5
vano <simplmd@gmail.com>
2026-02-22 12:09:19 +0700

    up

--- mix.exs
+++ mix.exs
@@ -4,10 +4,31 @@ defmodule ExCollision.MixProject do
   def project do
     [
       app: :ex_collision,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      name: "ExCollision",
+      deps: deps(),
+      description: "A library for server-side collisions, physics world simulation, tilemap pathfinding, and Tiled TMX parsing.",
+      package: package(),
+      source_url: "https://github.com/Valh88/ex_collision",
+      docs: docs(),
+    ]
+  end
+
+  defp package do
+    [
+      name: :ex_collision,
+      files: ["lib", "mix.exs", "README.md"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Valh88/ex_collision"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 
